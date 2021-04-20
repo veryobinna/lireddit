c67301c5497751508b768e93aff579a864bdfbae
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2021-04-20 09:51:05 +0100

    setup mikro orm

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { PROD } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM} from "@mikro-orm/core";
+import path from 'path';
+
+export default {
+    migrations:{
+        path: path.join(__dirname, './migrations'), // path to the folder with migrations
+        pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+        disableForeignKeys: false
+    },
+    entities:[Post],
+    dbName:'lireddit',
+    user: "my_user",
+    password:"root",
+    debug:!PROD,
+    type:"postgresql",
+
+} as Parameters<typeof MikroORM.init>[0];
