6d9597abf26a98eeab3f547520b0b4ac437a3ad8
Obinna Okwuolisa <o.okwuolisa@dickblick.com>
2021-04-21 11:06:42 +0100

    setup type-graphql

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,32 @@
+import "reflect-metadata";
 import { MikroORM} from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+import express from "express";
 import microConfig from "./mikro-orm.config"
+import {ApolloServer} from  "apollo-server-express";
+import { buildSchema } from "type-graphql";
+
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/posts";
 
 const main = async () =>{
     const orm = await MikroORM.init(microConfig) //connect to db
     await orm.getMigrator().up(); // run migration
-    // const post = orm.em.create(Post, {title:"my first post"})
-    // await orm.em.persistAndFlush(post)
-    const posts = await orm.em.find(Post, {})
-    console.log(posts)
+
+    const app = express()
+    // app.get("/", (_, res)=>{
+    //     res.send("Hello")
+    // })
+    const apolloServer = new ApolloServer({
+        schema : await buildSchema({
+            resolvers: [HelloResolver, PostResolver]
+        }),
+        context: () => ({em:orm.em})
+    });
+
+    apolloServer.applyMiddleware({app});
+    app.listen(4000, ()=>{
+        console.log("Server started on localhost")
+    })
 }
 main().catch(function(err){
     console.error(err)
